3550d85582d204bc7405c762713e40a0a97dd74e
Marcelo <henqpower2010@gmail.com~>
2026-08-20 14:49:33 -0300

    a

--- Aula-Grafo/questoes/loboMal.java
+++ Aula-Grafo/questoes/loboMal.java
@@ -1,5 +1,18 @@
 package questoes;
 
+import java.util.Scanner;
+
 public class loboMal {
-    
+    public final static Scanner S = new Scanner(System.in);
+    public static void main(String[] args) {
+        int colunas = S.nextInt();
+        int linhas = S.nextInt();
+        char[][] mapa = new char[colunas][linhas];
+        for (int i = 0; i < mapa.length; i++) {
+            for (int j = 0; j < mapa[0].length; j++) {
+                mapa[i][j] = S.next().charAt(0);                
+            }
+        }
+        
+    }
 }
--- BuscaBin/casas.java
+++ BuscaBin/casas.java
@@ -1,28 +1,35 @@
 package BuscaBin;
-import java.util.LinkedHashSet;
-import java.util.LinkedHashMap;
 import java.util.Scanner;
-import java.util.function.IntFunction;
-
 public class casas {
     final static public Scanner S = new Scanner(System.in);
     public static void main(String[] args) {
         int qtdCasas = S.nextInt();
-        LinkedHashSet<Integer> casas = new LinkedHashSet();
         int[] casasV = new int[qtdCasas];
         for (int i = 0; i < qtdCasas; i++) {
             casasV[i] = S.nextInt();
-            casas.add(casasV[i]);
         }
         int numSoma = S.nextInt();
-        int inicio = 0;
-        int fim = casasV.length - 1;
-        int meio = fim - ((inicio+fim)/2);
-        while (true) {
-            int Num2 = casasV[meio]-numSoma;
-            if (casas.contains(Num2)) {
-                System.out.println(meio+" "+Num2);
+        for (int i = 0; i < casasV.length; i++) {
+            int numQueFalta = numSoma-casasV[i];
+            int inicio = i+1;
+            int fim = casasV.length - 1;
+            boolean encotro = false;
+            while (inicio<=fim) {
+                int meio = fim - (inicio-fim)/2;
+                if (casasV[meio] == numQueFalta) {
+                    encotro = true;
+                    break;
+                }else if(casasV[meio]>numQueFalta){
+                    fim = meio-1;
+                } else{
+                    inicio = meio+1;
+                }
             }
+            if (encotro) {
+                System.out.println(casasV[i]+""+numQueFalta);
+                break;
+            } 
         }
+        
     }
 }
